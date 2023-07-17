7eac3cc138a59470510721ce88add39ac4a8bc3d
Nwaigba66 <glorianwaigba@gmail.com>
2023-07-16 18:28:56 -0700

    Updated pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,9 @@ class BaseModel:
     Base class for other models.
     Attributes:
         id (str): Unique identifier for the instance.
-        created_at (datetime): Date & time when the instance was created.
-        updated_at (datetime): Date & time when the instance was last updated.
+        created_at (datetime): Date & time the instance was created.
+        updated_at (datetime): Date & time instance was last updated.
     """
-
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
@@ -51,4 +50,3 @@ class BaseModel:
         result["created_at"] = self.created_at.isoformat()
         result["updated_at"] = self.updated_at.isoformat()
         return result
-

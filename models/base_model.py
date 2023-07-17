8c69810733a185339a6909ea2d32238873ce91e8
Nwaigba66 <glorianwaigba@gmail.com>
2023-07-16 18:06:41 -0700

    Updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,47 +6,49 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
-	"""
-	Base class for other models.
-	Attributes:
-	id (str): Unique identifier for the instance.
-	created_at (datetime): Date & time when the instance was created.
-	updated_at (datetime): Date & time when the instance was last updated.
-	 """
-	def __init__(self):
-        	self.id = str(uuid.uuid4())
-        	self.created_at = self.updated_at = datetime.now()
+    """
+    Base class for other models.
+    Attributes:
+        id (str): Unique identifier for the instance.
+        created_at (datetime): Date & time when the instance was created.
+        updated_at (datetime): Date & time when the instance was last updated.
+    """
 
-	def __str__(self):
-         	"""
-        	Returns a string representation of the BaseModel instance.
-		"""
-		return f"[{self.__class__.__name__}] ({self.id}) ({self.dict})"
-	
-	def save(self):
-        	"""
-        	Updates the updated_at attribute with the current date and time
-        	& saves it to storage
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
 
-        	This method is called to mark when an instance is updated.
-        	"""
-        	self.updated_at = datetime.now()
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
-	def to_dict(self):
-    		"""
-        	Returns a dictionary representation of the BaseModel instance.
+    def save(self):
+        """
+        Updates the updated_at attribute with the current date and time
+        & saves it to storage
 
-        	The dictionary contains all the attributes of the instance,
-        	including the class name, id, created_at, and updated_at converted
-        	to ISO 8601 format.
+        This method is called to mark when an instance is updated.
+        """
+        self.updated_at = datetime.now()
 
-        	Returns:
-            	dict: Dictionary representation of the instance.
-        	"""
-        	result = self.__dict__.copy()
-        	result["__class__"] = self.__class__.__name_
-        	result["created_at"] = self.created_at.isoformat()
-        	result["updated_at"] = self.updated_at.isoformat()
-		return result
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel instance.
+
+        The dictionary contains all the attributes of the instance,
+        including the class name, id, created_at, and updated_at converted
+        to ISO 8601 format.
+
+        Returns:
+            dict: Dictionary representation of the instance.
+        """
+        result = self.__dict__.copy()
+        result["__class__"] = self.__class__.__name__
+        result["created_at"] = self.created_at.isoformat()
+        result["updated_at"] = self.updated_at.isoformat()
+        return result
 

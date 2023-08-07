53047890e4990ee497241675ac7ca07dff8d67ea
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-07 15:58:32 -0700

    Update code

--- models/base_model.py
+++ models/base_model.py
@@ -15,32 +15,28 @@ class BaseModel:
         created_at (datetime): Date & time the instance was created.
         updated_at (datetime): Date & time instance was last updated.
     """
-	def __init__(self, *args, **kwargs):
-        	"""
-		Initializes a new instance of BaseModel.
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel.
 
-		The id is set to a unique identifier generated using UUID.
-		The created_at & updated_at attributes are set to current date & time.
-        	"""
-        	if kwargs:
-            		for key, value in kwargs.items():
-                		if key == "__class__":
-                  			# skip this attribute
-                    			continue
-                		if key == "created_at" or key == "updated_at":
-                    			# convert value from string to datetime object
-                    			value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+        The id is set to a unique identifier generated using UUID.
+        The created_at & updated_at attributes are set to current date & time.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    # skip this attribute
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    # convert value from string to datetime object
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
 
-                			# set attribute of object using key & value
-                			setattr(self, key, value)
-        	else:
-            		self.id = str(uuid.uuid4())
-            		self.created_at = datetime.now()
-            		self.updated_at = self.created_at
-
-            		# save the new instance to storage
-            		models.storage.new(self)
-
+                # set attribute of object using key & value
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -48,7 +44,7 @@ class BaseModel:
         """
         return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
-def save(self):
+    def save(self):
         """
         Updates the updated_at attribute with the current date and time
         & saves it to storage

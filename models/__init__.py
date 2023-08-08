830a52ecbdbd75faad9b65a9483399fe2d1ce9b5
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-07 18:51:12 -0700

    Added new files

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""entry point for models directory"""
 
+
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -52,6 +53,7 @@ class BaseModel:
         This method is called to mark when an instance is updated.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """

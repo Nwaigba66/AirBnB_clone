f2e5b8ddfcfec8e56d710d0293f7d45bb2c01206
Nwaigba66 <glorianwaigba@gmail.com>
2023-08-07 18:47:04 -0700

    Update code

---
+++ models/engine/file_storage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import json
+import models
+""" storing and reloading objects in json file """
+
+
+class FileStorage():
+    __file_path = "file.json"
+    __objects = {}
+
+    """ dictionary for object"""
+    def all(self):
+        return FileStorage.__objects
+
+    """ adding a new object inside the dictionary of objects """
+    def new(self, obj):
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    """ saves objects inside a json file """
+    def save(self):
+        objects = {}
+        for k, v in FileStorage.__objects.items():
+            objects[k] = v.to_dict()
+            with open(FileStorage.__file_path, "W") as f:
+                f.write(json.dumps(objects))
+
+    """ reloads dictionary from a json file """
+    def reload(self):
+        dct = {'BaseModel': BaseModel}
+        try:
+            with open(FileStorage.__file_path, "r") as f:
+                dic = json.loads(f.read())
+                for k, v in dic.items():
+                    obj = dct[value["__class__"]](**v)
+                    FileStorage.__objects[k] = obj
+        except FileNotFoundError:
+            pass
